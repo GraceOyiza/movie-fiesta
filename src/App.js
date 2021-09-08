22928d54321c4906b96e89dcb4ceaed75cb313b5
GraceOyiza <graceoyiza13@gmail.com>
2021-09-08 05:19:54 +0100

    add navbar for navigation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage';
 import MoviePage from './pages/Video';
+import NotFound from './pages/NotFound';
+import NavBar from './components/Navbar';
 
 const App = () => (
   <Router>
     <div className="App max-width">
+      <NavBar />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/movie" component={MoviePage} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>
